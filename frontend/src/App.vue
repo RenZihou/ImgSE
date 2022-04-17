432c8c6aafd1dabf6d4958a4ed72ce7a5a6ce6eb
Ren Zihou <renzihou.bj@gmail.com>
2022-04-17 15:46:06 +0800

    tag display & better layout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,36 @@
 <template>
-  <div class="search-bar">
-    <q-field borderless class="search-input">
-      <q-input v-model="this.query" rounded outlined clearable clear-icon="close" placeholder="search"/>
-      <q-btn rounded flat icon="search" @click="search"/>
-    </q-field>
-  </div>
-  <div class="search-result q-pa-md row items-start q-gutter-md">
-    <q-card v-for="(g, k) in gallery" :key="k" class="image-card">
-      <img :src="parseImgUrl(g.image_id)" alt="">
-      <q-card-section>
-        <div class="image-desc">
-          {{ g.desc }}
+  <div class="main">
+    <div class="search-bar items-center">
+      <q-field borderless class="search-input">
+        <div class="col">
+          <q-input v-model="this.query" outlined clearable clear-icon="close" placeholder="search"
+                   @keydown.enter.prevent="search">
+            <q-btn flat icon="search" @click="search"/>
+          </q-input>
         </div>
-      </q-card-section>
-    </q-card>
+      </q-field>
+    </div>
+    <div class="row items-start justify-around q-gutter-y-xl search-result">
+      <q-card v-for="(g, k) in gallery" :key="k" class="image-card">
+        <img :src="parseImgUrl(g.image_id)" alt="">
+        <q-card-section class="q-pa-md q-gutter-md image-tag">
+          <q-badge outline color="primary" v-for="t in g.tags" :key="t" :label="t"/>
+        </q-card-section>
+        <q-card-actions>
+          <q-space/>
+          <q-btn flat :icon="g.expanded ? 'keyboard_arrow_up' : 'keyboard_arrow_down'" color="grey"
+                 @click="g.expanded = !g.expanded"/>
+        </q-card-actions>
+        <q-slide-transition>
+          <div v-show="g.expanded">
+            <q-separator/>
+            <q-card-section class="image-desc">
+              {{ g.desc }}
+            </q-card-section>
+          </div>
+        </q-slide-transition>
+      </q-card>
+    </div>
   </div>
 </template>
 
@@ -21,11 +38,7 @@
 import axios from 'axios';
 
 export default {
-  name: 'LayoutDefault',
-
-  components: {
-    // HelloWorld
-  },
+  name: 'MainLayout',
 
   methods: {
     search() {
@@ -33,6 +46,9 @@ export default {
       axios.get(path, {params: {query: this.query}})
           .then(resp => {
             if (resp.status === 200) {
+              Array.from(resp.data.data).forEach(each => {
+                each.expanded = false;
+              });
               this.gallery = resp.data.data;
             } else {
               console.log(resp.status);
